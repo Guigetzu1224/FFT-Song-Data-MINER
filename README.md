9e6fc28b6bfa5b1ddfa04a26e02e0ab6181cde82
Samuel Stein <Samuelstein1224@gmail.com>
2019-10-03 16:50:16 -0400

    Adding a lot of functionatity such as wav converters, and new approach to classification

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ The fundamental basis of this is done on fourier transforms and frequency ranges
 Coming implimentations:
 Song classification
 Song recommendor based on Genre --> R^2 value between the two songs
+
+
+DO NOT RUN STRIP_WAV MORE THAN ONCE. It will corrupt file names for the python script
--- venv/main.py
+++ venv/main.py
@@ -4,6 +4,8 @@ from scipy.io import wavfile
 from scipy import stats
 import numpy as np
 import math
+import time
+import pandas as pd
 
 time_1 = 60
 time_2 = 150
@@ -21,13 +23,12 @@ def freq_band(frq):
         return 'Upper Midrange'
     elif frq < 6000:
         return 'Presence'
-    elif frq < 200000:
+    elif frq < 20000:
         return 'Brilliance'
     elif frq > 20000:
         return 'Out of human hearing'
 
 def band_generator(freq,intensity):
-    band = freq_band(freq)
     DataSet = set(zip(freq,intensity))
     Subbass = []
     Bass = []
@@ -55,7 +56,7 @@ def band_generator(freq,intensity):
             Brilliance.append([data[0],data[1],b_point])
         else:
             NA.append([data[0],data[1],b_point])
-    return [Subbass,Bass,Low_Midrange,Midrange,Upper_Midrange,Presence,Brilliance]
+    return [Subbass,Bass,Low_Midrange,Midrange,Upper_Midrange,Presence,Brilliance,NA]
 
 
 
@@ -77,17 +78,27 @@ def analyze_song(SONG_PATH):
     k = np.arange(len(real_fft_data))
     T = len(data) / bit_rate
     frq_axis = k / T
-    data_array = set(zip(frq_axis,log_fft))
-    frequency_list = []
-    for d_point in data_array:
-        band = freq_band(d_point[0])
-        frequency_list.append([d_point[0],d_point[1],band])
-
-    plt.plot(frq_axis,log_fft,'g')
+    means = []
+    audio_class = band_generator(frq_axis,log_fft)
+    for frq_band in audio_class:
+        frq_ax = [item[0] for item in frq_band]
+        frq_app = [item[1] for item in frq_band]
+        sum = 0
+        for item in frq_band:
+            sum += item[1]
+        if len(frq_band) > 0:
+            mean_y = sum/len(frq_band)
+        else:
+            mean_y = 0
+        means.append(mean_y)
+        rand_color = np.random.rand(3,)
+        plt.plot(frq_ax,frq_app,'o', c = rand_color)
+        plt.axhline(y=mean_y, c = rand_color, linestyle='-')
     plt.show()
+    plt.show()
+    return means
     slope, intercept, r_value, p_value, std_err = stats.linregress(frq_axis, log_fft)
  #   return [slope,intercept,r_value,p_value,std_err]
-    return frequency_list
-
+Cols = ['Subbass','Bass','Low Midrange','Midrange','Upper Midrange','Presence','Brilliance','Out of hearing']
 print(analyze_song('../Sexual_Healing.wav'))
 print(analyze_song('../Toto-Africa.wav'))
